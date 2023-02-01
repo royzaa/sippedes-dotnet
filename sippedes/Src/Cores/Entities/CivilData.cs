37d5292c19905e9526659ed0daa869ff3db0db81
Ar-Royan Zainul Akbar <arroyanzaa@gmail.com>
2023-02-01 09:11:17 +0700

    [FEATURE]: civil data entity

---
+++ sippedes/Src/Cores/Entities/CivilData.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using sippedes.Commons.Constants;
+
+namespace sippedes.Cores.Entities;
+
+[Table(name:"m_civil_data")]
+public class CivilData
+{
+    [Key, Column(name: "id")] public string NIK { get; set; }
+    
+    [Column(name: "no_kk"), Required]
+    public string NoKK { get; set; } = null!;
+
+    [Column(name: "fullname"), Required]
+    public string Fullname { get; set; } = null!;
+
+    [Column(name: "gender"), Required]
+    public string Gender { get; set; } = null!;
+
+    [Column(name: "blood_type")]
+    public string BloodType { get; set; }
+    
+    [Column(name: "education")]
+    public string Education { get; set; }
+    
+    [Column(name: "birthdate")]
+    public string BirthDate { get; set; }
+    
+    [Column(name: "address")]
+    public string Address { get; set; }
+    
+    [Column(name: "province")]
+    public string Province { get; set; }
+    
+    [Column(name: "city")]
+    public string City { get; set; }
+    
+    [Column(name: "district")]
+    public string District { get; set; }
+    
+    [Column(name: "village")]
+    public string Village { get; set; }
+    
+    [Column(name: "religion")]
+    public string Religion { get; set; }
+    
+}
