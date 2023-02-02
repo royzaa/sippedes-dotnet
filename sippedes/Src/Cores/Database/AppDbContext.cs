31b5e9c641d490c78252ed4af8522f711dc30e88
Ar-Royan Zainul Akbar <arroyanzaa@gmail.com>
2023-02-02 14:55:46 +0700

    [FEATURE] letter model

--- sippedes/Src/Cores/Database/AppDbContext.cs
+++ sippedes/Src/Cores/Database/AppDbContext.cs
@@ -6,13 +6,17 @@ namespace sippedes.Cores.Database;
 public class AppDbContext : DbContext
 {
 
-    // public DbSet<CivilData> CivilDatas => Set<CivilData>();
-    // public DbSet<Otp> Otps => Set<Otp>();
-    // public DbSet<UserCredential> UserCredentials => Set<UserCredential>();
-    // public DbSet<Role> Roles => Set<Role>();
-    // public DbSet<AdminData> AdminDatas => Set<AdminData>();
+    public DbSet<CivilData> CivilDatas => Set<CivilData>();
+    public DbSet<Otp> Otps => Set<Otp>();
+    public DbSet<UserCredential> UserCredentials => Set<UserCredential>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<AdminData> AdminDatas => Set<AdminData>();
 
     public DbSet<DeviceTokenNotification> DeviceTokenNotifications => Set<DeviceTokenNotification>();
+    public DbSet<Letter> Letters => Set<Letter>();
+    public DbSet<LetterCategory> LetterCategories => Set<LetterCategory>();
+    public DbSet<TrackingStatus> TrackingStatus => Set<TrackingStatus>();
+    
 
     protected AppDbContext()
     {
